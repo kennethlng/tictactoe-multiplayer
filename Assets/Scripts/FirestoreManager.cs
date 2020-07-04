17f7f35ce4aa156fd64ce5f759d066a6e84ead91
kennethlng <ng.kennethlee@gmail.com>
2020-07-03 20:38:59 -0700

    Setup

---
+++ Assets/Scripts/FirestoreManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase.Firestore;
+using Firebase.Extensions; 
+
+public class FirestoreManager : MonoBehaviour
+{
+    private FirebaseFirestore db = FirebaseFirestore.DefaultInstance;
+
+    private static FirestoreManager _instance;
+    public static FirestoreManager Instance {  get { return _instance; } }
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject); 
+        }
+        else
+        {
+            _instance = this; 
+        }
+    }
+}
---
+++ Assets/Scripts/Match.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Match
+{
+    public int id;
+    public bool isActive;
+    public string playerO;
+    public string playerX;
+    public string winner; 
+}
