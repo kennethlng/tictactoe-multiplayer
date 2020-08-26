d386c8ddeb1766556191c9ed0081b745a18bad3d
kennethlng <ng.kennethlee@gmail.com>
2020-08-25 18:48:59 -0700

    update

---
+++ Assets/Scripts/ScriptableObjects/ColorVariable.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+[CreateAssetMenu]
+public class ColorVariable : ScriptableObject
+{
+    public Color Value; 
+}
